81f41e62b84712e2fcd1460734d7d59254cf0b60
Tagore-7 <75824014+Tagore-7@users.noreply.github.com>
2025-03-11 07:38:34 -0400

    leetcode_daily_3/11/2025

---
+++ 1358NumberofSubstringsContainingAllThreeCharacters.py
@@ -0,0 +1,44 @@
+#Given a string s consisting only of characters a, b and c.
+#
+#Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+#
+# 
+#
+#Example 1:
+#
+#Input: s = "abcabc"
+#Output: 10
+#Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again). 
+#Example 2:
+#
+#Input: s = "aaacb"
+#Output: 3
+#Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+#Example 3:
+#
+#Input: s = "abc"
+#Output: 1
+# 
+#
+#Constraints:
+#
+#3 <= s.length <= 5 x 10^4
+#s only consists of a, b or c characters.
+#
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        res = 0
+        abc_count = defaultdict(int)
+        left = 0
+
+        for right in range(len(nums)):
+            abc_count[s[right]] += 1
+
+            while len(abc_count) == 3:
+                res += (len(s) - right)
+                abc_count[s[left] -= 1]
+                if abc_count[s[left]] == 0:
+                    abc_count.pop(s[left])
+                left += 1
+
+        return res
